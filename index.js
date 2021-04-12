57ee40baff3a1a0ac62657d248546962c4e1fce3
Sheridan <sheridanmaustad@gmail.com>
2021-04-12 17:53:07 -0600

    Completed assignnment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(msg="roller-skate") {
+    return `This Saturday, I want to ${msg}!`;
+} 
+saturdayFun();
+
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+
+function wrapAdjective(msg="*"){
+    return function innerfunction (highlight="special"){
+        return `You are ${msg}${highlight}${msg}!`;
+    }
+}
